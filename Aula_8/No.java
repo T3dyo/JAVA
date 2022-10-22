0b4a48063a89939075ec446e3dd47dfdf27db6c6
T3dyo <116457677+T3dyo@users.noreply.github.com>
2022-10-22 15:45:53 -0300

    Create No.java

---
+++ Aula_8/No.java
@@ -0,0 +1,16 @@
+public class No{//atributos
+  int dado;
+  No prox;
+
+
+public No(int i){ // construtor
+  dado = i;
+  prox = null; 
+}
+
+public void imprime(){//metodo
+  System.out.println("Valor = "+dado);
+}
+
+  
+}
