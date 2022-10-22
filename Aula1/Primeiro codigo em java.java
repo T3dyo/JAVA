38d35c82c9fa0fbaccff1ffee3a74a2163776d70
T3dyo <116457677+T3dyo@users.noreply.github.com>
2022-10-22 15:17:28 -0300

    Create Primeiro codigo em java.java

---
+++ Aula1/Primeiro codigo em java.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+
+   int a=4, b=2;
+  calculadora (a,b);
+  }
+  
+    public static void calculadora(int a, int b)
+  {
+    System.out.println("\n A+B="+(a+b));//printf
+    System.out.println(" A-B="+(a-b));//sub
+    System.out.println(" A*B="+(a*b));//mult
+    System.out.println(" A/B="+(a/b));//divisao
+    System.out.println(" A%B="+(a%b));//resto
+  }
+}
