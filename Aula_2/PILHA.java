af3b4c01e79d320a881899262bc7e83166aed393
T3dyo <116457677+T3dyo@users.noreply.github.com>
2022-10-22 15:20:45 -0300

    Create PILHA.java

---
+++ Aula_2/PILHA.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    Pilha p = new Pilha(5);
+    p.empilha(1);
+    p.empilha(2);
+    p.empilha(3);
+    p.empilha(4);
+    p.empilha(5);
+    p.empilha(6); 
+    p.imprime();
+    //p.imprimeaocontrario(); 
+  }
+} 
+
