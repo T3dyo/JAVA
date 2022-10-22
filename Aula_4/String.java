5cf161652fe104d309b7cb394570dbb624020b93
T3dyo <116457677+T3dyo@users.noreply.github.com>
2022-10-22 15:32:01 -0300

    Create String.java
    Aparecera a primeira letra do nome 
    Aparecera a ultima letra do nome
    Mostra a quantidade de letras
    resp: M
             O
             16

---
+++ Aula_4/String.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String n = "MARCELO_MARVADO";
+    int len = n.length();
+    char primeiro = n.charAt(0);
+    char ultimo = n.charAt(len - 1);
+    System.out.println(primeiro + "\n" + ultimo + "\n" + len);
+  }
+}
